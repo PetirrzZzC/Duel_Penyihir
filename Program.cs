38ac559cd53226a6535ed3fef3ec533b3c3ce3d1
PetirrzZzC <ananda_prasetyo_34pg@student.smktelkom-mlg.sch.id>
2026-02-02 11:13:09 +0700

    OOP#1
    Menambah Class Wizard, dua obect Wizard, dan simulasi battle.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Wizard wizardA = new Wizard("Mak Lampir", 20);
+Wizard wizardB = new Wizard("Wicth", 30);
+
+Console.WriteLine("Permainan diMulai. . .\n");
+wizardA.ShowStats();
+wizardB.ShowStats();
+
+wizardA.attack(wizardB);
+wizardB.attack(wizardA);
+wizardA.attack(wizardB);
+
+Console.WriteLine("Permainan Berakhir. . .\n");
+wizardA.ShowStats();
+wizardB.ShowStats();
+
+class Wizard
+{
+    public string Name;
+    public int Energi;
+    public int Damage;
+
+    public Wizard(string name, int damage)
+    {
+        Name = name;
+        Energi = 100;
+        Damage = damage;
+    }
+
+    public void ShowStats()
+    {
+        Console.WriteLine("Statistik Wizard");
+        Console.WriteLine($"Nama: {Name}, Energi: {Energi}\n");
+    }
+
+    public void attack(Wizard wizardlawanObj)
+    {
+        wizardlawanObj.Energi -= Damage;
+        Console.WriteLine($"{Name} menyerang {wizardlawanObj.Name}");
+        Console.WriteLine($"Sisa energi {wizardlawanObj.Name} adalah {wizardlawanObj.Energi}");
+    }
+
+}
+
